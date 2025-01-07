76782096235ef47a0ede85915b7caf8938902408
joon shim <jshim4996@gmail.com>
2025-01-07 09:20:41 +0900

    Rust 가이드북 1장 학습

---
+++ rust/basic/projects/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646.d
@@ -0,0 +1,5 @@
+/Users/joonshim/JS/study/rust/basic/projects/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646: src/main.rs
+
+/Users/joonshim/JS/study/rust/basic/projects/hello_cargo/target/debug/deps/hello_cargo-c40274329a7a9646.d: src/main.rs
+
+src/main.rs:
---
+++ rust/basic/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/joonshim/JS/study/rust/basic/projects/hello_cargo/target/debug/hello_cargo: /Users/joonshim/JS/study/rust/basic/projects/hello_cargo/src/main.rs
