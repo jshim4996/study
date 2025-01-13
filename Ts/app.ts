57e3cdb5b9be77abdbd36f8cf6ba15279a3488fa
joon shim <jshim4996@gmail.com>
2025-01-13 23:03:47 +0900

    유데미 ts 강좌 ojbect, tupple, enum 타입 학습함

--- Ts/app.ts
+++ Ts/app.ts
@@ -1,15 +1,47 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  if (showResult) console.log(phrase + (n1 + n2));
-  else return n1 + n2;
+// const person: {
+//   name: string;
+//   age: number;
+// }
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; // => [타입, 타입.....] 튜플타입 사용 할 때
+} = {
+  name: "심준",
+  age: 31,
+  hobbies: ["코딩", "운동", "독서"],
+  role: [2, "author"],
+};
+
+//person.role.push("admin"); // 튜플타입 오류라면 push 로 값을 추가하는것이 가능하다. => 이것은 주의 필요
+//person.role[2] = 1; // => 튜플 타입이 아니라면 stirng 타입의 요소값이 숫자형으로 변경 할 수 있다. 단 튜플 타입이라면 오류
+
+//person.role = [1, "admin", 'test']; // => 만약 튜플 타입에 새로운 값을 할당 할때 지정된 길이와 요소 타입이 맞지 않다면 오류가 발생한다.
+
+let favoriteActivities: string[];
+favoriteActivities = ["운동"];
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
 }
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
+enum Jobs {
+  WARRIOR = 50,
+  MAGE,
+  ARCHER,
+}
 
-let testData; // any 타입
-testData = "5"; // any 타입
+const user = {
+  id: "jshim",
+  lavel: 50,
+  job: Jobs.MAGE,
+};
 
-const result = add(number1, number2, printResult, resultPhrase);
-// console.log(result);
+if (user.job === Jobs.MAGE) {
+  console.log("마법사 직업니다.");
+  console.log(user.job); // => 자동으로 1이라는 값을 MAGE 에 매핑 해준다.
+}
+
+console.log(Jobs.WARRIOR);
